79344fb493b04f001b8ee2acf86d9850e7a47dbc
Emma <enabojohn@gmail.com>
2020-03-12 14:02:52 +0300

    Innitial commit, Created online DB connection,.env file and post route

---
+++ app.js
@@ -0,0 +1,44 @@
+//DESCRIPTION:Restful Api with Nodejs, Express and Mongo Db
+
+const express = require('express');
+const mongoose = require("mongoose");
+// Innitialising express; Provides the ability to create routes
+const app = express();
+require('dotenv/config');
+
+
+//Import routes
+const postsRoute = require('./routes/posts')
+app.use('/posts', postsRoute);
+
+
+
+//Mongo DB Database connection
+mongoose.connect(
+    process.env.DB_CONNECTION,
+{ useNewUrlParser: true },
+ () => 
+    console.log("Connected to database")
+  );
+
+//ROUTE(patch updates a post)
+app.get('/', (req, res) => {
+    res.send('We are on home')
+});
+
+
+//Listening to the server
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
